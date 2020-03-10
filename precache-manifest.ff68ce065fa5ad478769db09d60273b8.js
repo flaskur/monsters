74847ff90a1859936dea2fe7246fa04c9be59e81
flaskur <flaskur@gmail.com>
2020-03-10 13:07:03 -0400

    Updates

---
+++ precache-manifest.ff68ce065fa5ad478769db09d60273b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6324080b2f68646cb18320cb00e13681",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "92260d2d984c0665d562",
+    "url": "/monsters/static/css/main.859c8b9d.chunk.css"
+  },
+  {
+    "revision": "46dc878f4934852227da",
+    "url": "/monsters/static/js/2.3f3242da.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.3f3242da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92260d2d984c0665d562",
+    "url": "/monsters/static/js/main.dc5e2c27.chunk.js"
+  },
+  {
+    "revision": "8eb099a4c348945aa682",
+    "url": "/monsters/static/js/runtime-main.3e7da30a.js"
+  }
+]);
